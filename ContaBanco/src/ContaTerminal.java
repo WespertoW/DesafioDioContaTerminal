450a21d3b6670e9f8a804373cd584d17ef3e1a27
WespertoW <ghreinocraft@gmail.com>
2024-11-20 15:21:46 -0300

    Finalização Desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         //TODo: Conhecer e importar a classe Scanner
@@ -13,5 +15,28 @@ public class ContaTerminal {
         /*SAIDA: Olá [Nome do Cliente] obrigado por criar uma conta em nosso banco, sua agência é [Agencia], 
         conta [Numero Conta] e seu saldo [Saldo] já está disponivel para saque.
         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("----------------------");
+        System.out.println("   Criação da Conta   ");
+        System.out.println("----------------------");
+
+        System.out.println("Olá, Digite seu nome para continuarmos.");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Informe o numero da Agência ! (ex: 0101)");
+        String numeroAgencia = scanner.nextLine();
+
+        System.out.println("Agora digite o numero da da conta ! (ex: 0010123-1)");
+        String numeroConta = scanner.nextLine();
+
+        System.out.println("Informe Seu Saldo ? (ex: 10,10)");
+        double saldoCliente = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.printf("Olá %s obrigado por criar uma conta em nosso banco, sua agência é %s, e o numero da sua conta é %s, e seu saldo R$: %.2f já está disponivel para saque.", nomeCliente, numeroAgencia, numeroConta, saldoCliente);
+        
     }
 }
